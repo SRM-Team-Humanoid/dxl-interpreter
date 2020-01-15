735f67b928c60b5216fbf82f6ff78857fa83303c
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2020-01-16 01:06:51 +0530

    started off

--- dxlint.py
+++ dxlint.py
@@ -3,17 +3,20 @@ import pypot.dynamixel
 
 def init():
     ports=pypot.dynamixel.get_available_ports()     
-    print(ports)
+    print (ports)
     if not ports :
         raise IOError("no ports found bruh!")
-    print "Connecting to ",ports[0]
+    print ("Connecting to ",ports[0])
     global dxl
     dxl=pypot.dynamixel.DxlIO(ports[0])
     ids=dxl.scan(range(30))
-    print ids
-    print dxl.get_present_position(ids)
-    dxl.set_moving_speed({1:100,3:100,5:100})
-    dxl.set_goal_position(notes['E'])
+    print (ids)
+    print (dxl.get_present_position(ids))
+
+
+def movstmt(name, anglelist):
+    dxl.set_goal_position(dict(zip((variables[name],anglelist))))
+
 
 
 if __name__=='__main__':
